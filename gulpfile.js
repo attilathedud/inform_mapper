5a81b010a7acb99a2f625133b23955b2d5bbd06e
Nathan Tucker <abezhat@verizon.net>
2017-10-14 12:05:48 -0700

    Adding in sass support, move frontpage to sass format

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
+var minify = require('gulp-minify-css');
+var sass = require('gulp-sass');
+var merge = require('merge-stream');
+var plumber = require('gulp-plumber');
 
 gulp.task('styles', function() {
-	gulp.src('inform_mapper/static/css/*.css')
-		.pipe(minifyCSS())
+	var scss_stream = gulp.src('inform_mapper/static/scss/*.scss')
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(concat('scss-files.scss'));
+
+	var css_stream = gulp.src('inform_mapper/static/css/*.css')
+		.pipe(plumber())
+		.pipe(concat('css-files.css'));
+
+	var mergedStream = merge(scss_stream, css_stream)
+		.pipe(plumber())
 		.pipe(concat('style.min.css'))
-		.pipe(gulp.dest('inform_mapper/static/css'))
+		.pipe(minify())
+		.pipe(gulp.dest('inform_mapper/static/css'));
+
+	return mergedStream;
 });
 
 gulp.task('watch', function () {
-	gulp.watch(['inform_mapper/static/css/*.css', '!inform_mapper/static/css/style.min.css'], ['styles']);
+	gulp.watch(['inform_mapper/static/css/*.css', 'inform_mapper/static/scss/*.scss', '!inform_mapper/static/css/style.min.css'], ['styles']);
 });
 
 gulp.task('default', ['styles']);
