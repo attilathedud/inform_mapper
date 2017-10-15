a8ef3c491712b7165c3991997695f3e5ec9a8413
Nathan Tucker <abezhat@verizon.net>
2017-10-15 16:03:04 -0700

    Adding in search on info panel

--- inform_mapper/static/scripts/graph.js
+++ inform_mapper/static/scripts/graph.js
@@ -114,10 +114,10 @@
     
     for (var i = 0; i < accordion_elements.length; i++) {
         accordion_elements[i].addEventListener( 'click', function( e ) {
-            this.classList.toggle("active");
+            this.classList.toggle( 'active' );
             var panel = this.nextElementSibling;
     
-            if (panel.style.maxHeight) {
+            if ( panel.style.maxHeight ) {
                 panel.style.maxHeight = null;
             } else {
                 panel.style.maxHeight = panel.scrollHeight + "px";
@@ -127,7 +127,61 @@
     
     /* Wire up search box */
     var search_box = document.getElementById('search-box');
-
     search_box.focus();
+
+    document.addEventListener( 'click', function( e ) {
+		search_box.focus();
+    })
+    
+    var finder;
+
+    search_box.addEventListener( 'keypress', function( e ) {
+        if( e.which == 13 ) {
+            if( finder ) {
+                finder.revert();
+            }
+
+            if( search_box.value.replace(' ', '').length > 0 ) {
+                finder = findAndReplaceDOMText(document.getElementById('info-box'), {
+                    find: new RegExp(search_box.value, "gi"),
+                    wrap: 'span',
+                    wrapClass: 'highlight'
+                });
+    
+                for( var i = 0; i < accordion_elements.length; i++ ) {
+                    if( accordion_elements[ i ].classList.contains( 'active' ) ) {
+                        accordion_elements[ i ].click();
+                    }
+                }
+    
+                var word_panel_set = false;
+                var object_panel_set = false;
+                var selections = document.getElementsByClassName( 'highlight' );
+
+                for( var i = 0; i < selections.length; i++ ) {
+                    var cur_selection_parent = selections[ i ];
+    
+                    while( cur_selection_parent && cur_selection_parent.classList &&
+                        (!cur_selection_parent.classList.contains( 'panel' ) && !cur_selection_parent.classList.contains( 'accordion' ) ) ) {
+                            if( !word_panel_set && cur_selection_parent.classList.contains( 'word-panel' )) {
+                                cur_selection_parent.scrollTop = selections[ i ].offsetTop - cur_selection_parent.offsetTop;
+                                word_panel_set = true;
+                            }
+        
+                            if( !object_panel_set && cur_selection_parent.classList.contains( 'object-panel' )) {
+                                cur_selection_parent.scrollTop = selections[ i ].offsetTop - cur_selection_parent.offsetTop;
+                                object_panel_set = true;
+                            }
+                            
+                            cur_selection_parent = cur_selection_parent.parentNode;
+                    }
+    
+                    if( !cur_selection_parent.parentNode.getElementsByClassName('accordion')[0].classList.contains( 'active' ) ) {
+                        cur_selection_parent.parentNode.getElementsByClassName('accordion')[0].click();
+                    }   
+                }
+            }
+        }
+    });
     
 }(document, window));
