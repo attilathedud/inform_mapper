9262f9ba47e3951687653efce495f24ae78f6004
Nathan Tucker <abezhat@verizon.net>
2017-10-21 15:26:56 -0700

    Adding dist build, moving cytoscape to cdn

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,12 @@ var merge = require('merge-stream');
 var plumber = require('gulp-plumber');
 var del = require('del');
 var uglify = require('gulp-uglify');
-var fs = require('file-system');
 
 gulp.task('clean_styles', function () {
-    return del('inform_mapper/static/css/style.min.css');
+	return del('inform_mapper/static/css/style.min.css');
 });
 
-gulp.task('styles', ['clean_styles'],  function() {
+gulp.task('styles', ['clean_styles'], function () {
 	var scss_stream = gulp.src('inform_mapper/static/scss/*.scss')
 		.pipe(plumber())
 		.pipe(sass())
@@ -31,64 +30,48 @@ gulp.task('styles', ['clean_styles'],  function() {
 	return merged_stream;
 });
 
-gulp.task('clean_front_js', function() {
+gulp.task('clean_front_js', function () {
 	return del('inform_mapper/static/scripts/frontpage.min.js');
 });
 
-gulp.task('frontpage_scripts', ['clean_front_js'], function() {
+gulp.task('frontpage_scripts', ['clean_front_js'], function () {
 	return gulp.src(['inform_mapper/static/scripts/frontpage/modernizr-custom.js', 'inform_mapper/static/scripts/frontpage/frontpage.js'])
 		.pipe(concat('frontpage.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('inform_mapper/static/scripts/'));
 });
 
-gulp.task('clean_external_graph_js', function() {
-	return del('inform_mapper/static/scripts/graph/external/external.min.js');
-});
-
-function generate_external_scripts( ) {
-	return gulp.src([ 'inform_mapper/static/scripts/graph/external/*.js' ])
-		.pipe(concat('external.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('inform_mapper/static/scripts/graph/external/'));
-}
-
-gulp.task('external_graph_scripts', ['clean_external_graph_js'], function() {
-	return generate_external_scripts( );
-});
-
-gulp.task('clean_graph_js', function() {
+gulp.task('clean_graph_js', function () {
 	return del('inform_mapper/static/scripts/graph.min.js');
 });
 
-gulp.task('graph_scripts', ['clean_graph_js'], function() {
-	try {
-		fs.accessSync('inform_mapper/static/scripts/graph/external/external.min.js');
-	} catch( ex ) {
-		generate_external_scripts( );
-	}
-	
-	var external_stream = gulp.src([ 'inform_mapper/static/scripts/graph/external/external.min.js' ])
-		.pipe( plumber() );
-
-	var internal_stream = gulp.src(['inform_mapper/static/scripts/graph/graph_logic.js', 'inform_mapper/static/scripts/graph/graph.js'])
-		.pipe( plumber() )
-		.pipe(concat('internal.min.js'))
-		.pipe(uglify());
-
-	var merged_stream = merge( external_stream, internal_stream )
-		.pipe( plumber() )
+gulp.task('graph_scripts', ['clean_graph_js'], function () {
+	return gulp.src(['inform_mapper/static/scripts/graph/external/*.js', 'inform_mapper/static/scripts/graph/graph_logic.js', 'inform_mapper/static/scripts/graph/graph.js'])
+		.pipe(plumber())
 		.pipe(concat('graph.min.js'))
+		.pipe(uglify())
 		.pipe(gulp.dest('inform_mapper/static/scripts/'));
-
-	return merged_stream;
 });
 
 gulp.task('watch', function () {
 	gulp.watch(['inform_mapper/static/css/*.css', 'inform_mapper/static/scss/*.scss', '!inform_mapper/static/css/style.min.css'], ['styles']);
 	gulp.watch(['inform_mapper/static/scripts/frontpage/*.js'], ['frontpage_scripts']);
-	gulp.watch(['inform_mapper/static/scripts/graph/external/*.js', '!inform_mapper/static/scripts/graph/external/external.min.js'], ['external_graph_scripts']);
-	gulp.watch(['inform_mapper/static/scripts/graph/*.js'], ['graph_scripts']);
+	gulp.watch(['inform_mapper/static/scripts/graph/**/*.js'], ['graph_scripts']);
 });
 
-gulp.task('default', ['styles', 'frontpage_scripts', 'external_graph_scripts', 'graph_scripts' ]);
+gulp.task('clean_dist', function () {
+	return del('dist/');
+});
+
+gulp.task('dist', ['default', 'clean_dist'], function () {
+	var dist_files = ['inform_mapper/static/css/style.min.css', 'inform_mapper/static/imgs/*.*', 
+		'inform_mapper/static/scripts/*.min.js', 'inform_mapper/static/style/graph.json', 
+		'inform_mapper/static/test_games/*.*', 'inform_mapper/templates/**/*.*', 
+		'inform_mapper/inform_mapper.py', 'inform_mapper/inform_text.py', 'inform_mapper/inform_tools.py',
+		'inform_mapper/__init__.py' ];
+
+	gulp.src(dist_files, { base: 'inform_mapper/' })
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['styles', 'frontpage_scripts', 'graph_scripts']);
